fd6dbb96f59724d4648671526d4182542e1e6119
bopaki <btebalo@gmail.com>
2023-06-13 08:53:11 +0200

    added navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
             <ul>
                 <li>
-                    <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <a href="/longtermpermit" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
                         <div className="font-semibold">Apply for long-term work permits</div>
                         <span className="text-sm text-gray-500 dark:text-gray-400">Application fee is P2500.00 for a 3 year period.</span>
                     </a>
