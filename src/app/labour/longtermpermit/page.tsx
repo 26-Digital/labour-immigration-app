311b99d79b4a7b410bad89c63b91afed70f438b9
bopaki <btebalo@gmail.com>
2023-06-13 10:49:51 +0200

    uod

--- src/app/labour/longtermpermit/page.tsx
+++ src/app/labour/longtermpermit/page.tsx
@@ -1,33 +1,56 @@
 export default function LongTermPermit(){
     return(
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>     
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            
                 
-<form>
+    <form>
+  <div className="grid grid-cols-3 gap-4">
   <div className="mb-6">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
     <input 
-    type="email" 
-    id="email" 
-    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
     />
   </div>
   <div className="mb-6">
-    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
     <input 
-    type="password" 
-    id="password" 
-    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
     />
   </div>
   <div className="mb-6">
-    <label 
-    htmlFor="repeat-password" 
-    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
     <input 
-    type="password" 
-    id="repeat-password" 
-    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
+    />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
+    <input 
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
+    />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
+    <input 
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
+    />
+  </div>
+  <div className="mb-6">
+    <label htmlFor="applicant_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applicant name</label>
+    <input 
+    type="text" 
+    id="applicant_name" 
+    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required
     />
   </div>
   <div className="flex items-start mb-6">
@@ -41,9 +64,8 @@ export default function LongTermPermit(){
     <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
   </div>
   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+  </div>
 </form>
-
-            </div>
         </main>
     ) 
 }
